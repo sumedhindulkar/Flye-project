e9d2fd0a2b31c4502badf9a2bf6e33a6be4f5ced
sumedhindulkar <55879698+sumedhindulkar@users.noreply.github.com>
2021-01-13 00:01:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+  function myFunction() {
+    var element1 = document.getElementById("view");
+    var element2 = document.getElementById("hide");
+    element1.classList.toggle("view-inactive");
+    element2.classList.toggle("view-inactive");
+  }
